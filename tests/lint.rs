96b102b52a59136b5779604d9bf9773ba970cd1f
Julian Hyde <jhyde@apache.org>
2025-08-17 16:43:24 -0700

    Lint: Check for trailing spaces

--- tests/lint.rs
+++ tests/lint.rs
@@ -15,6 +15,8 @@
 // language governing permissions and limitations under the
 // License.
 
+use phf::{Map, Set, phf_map, phf_set};
+use regex::Regex;
 use std::fs;
 
 #[test]
@@ -44,8 +46,8 @@ fn lint() {
     );
 }
 
-fn header(line_prefix: &str) -> String {
-    let raw = r#"^Licensed to Julian Hyde under one or more contributor license
+static RAW_HEADER: &str =
+    r#"^Licensed to Julian Hyde under one or more contributor license
 ^agreements.  See the NOTICE file distributed with this work
 ^for additional information regarding copyright ownership.
 ^Julian Hyde licenses this file to you under the Apache
@@ -62,44 +64,74 @@ fn header(line_prefix: &str) -> String {
 ^language governing permissions and limitations under the
 ^License.
 "#;
-    raw.replace("^\n", format!("{}\n", line_prefix).as_str())
+
+fn header_for_suffix(line_prefix: &str) -> String {
+    RAW_HEADER.replace("^\n", format!("{}\n", line_prefix).as_str())
         .replace("^", format!("{} ", line_prefix).as_str())
 }
+
+static REGEX: std::sync::LazyLock<Regex> =
+    std::sync::LazyLock::new(|| Regex::new(r" +\n").unwrap());
 
 /// Checks that a file starts with a header comment.
 ///
 /// Appends a warning if not.
 fn lint_file(file_name: &str, warnings: &mut Vec<String>) {
-    let header = header_for(file_name);
-    if header.is_some() {
-        let contents = fs::read_to_string(file_name);
-        if !contents.unwrap().starts_with(header.unwrap().as_str()) {
+    let file_type = FileType::for_file(file_name);
+    if file_type.header.is_some() {
+        let contents = fs::read_to_string(file_name).unwrap();
+        if !contents.starts_with(file_type.header.unwrap().as_str()) {
             warnings.push(format!(
                 "{}:{}: File does not start with a header",
                 file_name, 1
             ));
         }
     }
-}
-
-/// Returns the appropriate header for a file based on its extension,
-/// or `None` if no header is needed.
-fn header_for(file_name: &str) -> Option<String> {
-    use phf::{Map, phf_map};
-
-    static SUFFIX_MAP: Map<&'static str, &'static str> = phf_map! {
-        "gitignore" => "#",
-        "rs" => "//",
-        "pest" => "//",
-        "toml" => "#",
-    };
-
-    let suffix = file_name.split('.').last();
-    if suffix.is_some() {
-        let option = SUFFIX_MAP.get(suffix.unwrap());
-        if option.is_some() {
-            return Some(header(option.unwrap()));
-        }
+    if file_type.text {
+        let contents = fs::read_to_string(file_name).unwrap();
+        let mut line = 0;
+        contents.lines().for_each(|l| {
+            line += 1;
+            if l.ends_with(' ') {
+                warnings.push(format!("{}:{}: Trailing spaces", file_name, line));
+            }
+        });
     }
-    None
 }
+
+/// Describes whether a file is text, and its required header, if any.
+struct FileType {
+    header: Option<String>,
+    text: bool,
+}
+
+impl FileType {
+    /// Returns the appropriate header for a file based on its extension,
+    /// or `None` if no header is needed.
+    fn for_file(file_name: &str) -> Self {
+        let suffix = file_name.split('.').last();
+        let mut header: Option<String> = None;
+        let mut text = false;
+        if suffix.is_some() {
+            let option = SUFFIX_MAP.get(suffix.unwrap());
+            if option.is_some() {
+                header = Some(header_for_suffix(option.unwrap()));
+                text = true;
+            }
+        }
+        text = text || TYPE_MAP.contains(suffix.unwrap());
+        FileType { header, text }
+    }
+}
+
+static SUFFIX_MAP: Map<&'static str, &'static str> = phf_map! {
+    "gitignore" => "#",
+    "rs" => "//",
+    "pest" => "//",
+    "toml" => "#",
+    "yml" => "#",
+};
+
+/// File suffixes of files that are considered text files.
+static TYPE_MAP: Set<&'static str> =
+    phf_set! {"gitignore","rs","pest","toml","md",};
